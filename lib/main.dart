b5dfca0ead627e746574ea7f176b11f47b7d8a3a
vian <vianv31253@gmail.com>
2024-10-09 06:15:55 +0700

    menambahkan 2 button di halaman depan

--- lib/main.dart
+++ lib/main.dart
@@ -9,10 +9,61 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    onPressed: () {},
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.add_chart_sharp,
+                          size: 150,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 32.0),
+                          child: Text(
+                            'Hitung BMI',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20,),
+                          ),
+                        ),
+                      ],
+                    )),
+                SizedBox(
+                  height: 30, // <-- SEE HERE
+                ),
+                ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20))),
+                    onPressed: () {},
+                    child: Column(
+                      children: [
+                        Icon(
+                          Icons.abc_sharp,
+                          size: 150,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(bottom: 32.0),
+                          child: Text(
+                            'Glosarium \nKalkulator BMI',
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 20,),
+                          ),
+                        ),
+                      ],
+                    ))
+              ],
+            ),
+          ),
         ),
       ),
     );
