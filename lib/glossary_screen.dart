8c670efadc073d7a0ffca6f08aefb9251911d17f
vian <vianv31253@gmail.com>
2024-10-16 16:09:32 +0700

    penyesuaian tampilan web

--- lib/glossary_screen.dart
+++ lib/glossary_screen.dart
@@ -13,93 +13,202 @@ class GlossaryScreen extends StatelessWidget {
           ),
           backgroundColor: Colors.deepPurpleAccent,
           iconTheme: IconThemeData(color: Colors.white)),
-      body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: ListView(
-              children: <Widget>[
-                Padding(
-                  padding: const EdgeInsets.only(bottom: 16.0),
-                  child: Text(
-                    'Tentang BMI',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title:
-                        Text('BMI merupakan kependekan dari Body Mass Index atau Index Masa Tubuh'),
-                    leading: Icon(Icons.accessibility),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text(
-                        'BMI dihitung dari pembagian berat badan (dalam kg) dengan tinggi badan (dalam m)'),
-                    leading: Icon(Icons.accessibility),
-                  ),
-                ),
-                Card(
-                  child: ListTile(
-                    title: Text(
-                        'Nilai dari perhitungan BMI diklasifikasikan ke dalam 6 kelompok kategori'),
-                    leading: Icon(Icons.accessibility),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Text(
-                    'Klasifikasi nilai BMI',
-                    textAlign: TextAlign.center,
-                    style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
-                  ),
-                ),
-                Card(
-                  color: Colors.yellow,
-                  child: ListTile(
-                    title: Text('UNDERWEIGHT\nNilai BMI <18.5'),
-                    leading: Icon(Icons.adb),
-                  ),
-                ),
-                Card(
-                  color: Colors.greenAccent,
-                  child: ListTile(
-                    title: Text('NORMAL\n18.5 ≤ Nilai BMI < 25.0'),
-                    leading: Icon(Icons.adb),
-                  ),
-                ),
-                Card(
-                  color: Colors.yellow,
-                  child: ListTile(
-                    title: Text('OVERWEIGHT\n25 ≤ Nilai BMI < 30.0'),
-                    leading: Icon(Icons.adb),
-                  ),
-                ),
-                Card(
-                  color: Colors.amber,
-                  child: ListTile(
-                    title: Text('CLASS I OBESITY\n30 ≤ Nilai BMI < 35.0'),
-                    leading: Icon(Icons.adb),
-                  ),
-                ),
-                Card(
-                  color: Colors.orange,
-                  child: ListTile(
-                    title: Text('CLASS II OBESITY\n35 ≤ Nilai BMI < 40.0'),
-                    leading: Icon(Icons.adb),
-                  ),
-                ),
-                Card(
-                  color: Colors.redAccent,
-                  child: ListTile(
-                    title: Text('CLASS III OBESITY\nNilai BMI ≥ 40.0'),
-                    leading: Icon(Icons.adb),
-                  ),
-                ),
-              ],
-            ),
-          )),
+      body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth <= 750)
+          return GlossaryScreenList();
+        else if (constraints.maxWidth <= 950)
+          return GlossaryScreenGrid(gridCount: 3);
+        else if (constraints.maxWidth <= 1200)
+          return GlossaryScreenGrid(gridCount: 4);
+        else if (constraints.maxWidth <= 1400)
+          return GlossaryScreenGrid(gridCount: 5);
+        else
+          return GlossaryScreenGrid(gridCount: 6);
+      }),
     );
+  }
+}
+
+class GlossaryScreenList extends StatelessWidget {
+  const GlossaryScreenList({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.only(bottom: 16.0),
+            child: Text(
+              'Tentang BMI',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text(
+                  'BMI merupakan kependekan dari Body Mass Index atau Index Masa Tubuh'),
+              leading: Icon(Icons.accessibility),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text(
+                  'BMI dihitung dari pembagian berat badan (dalam kg) dengan tinggi badan (dalam m)'),
+              leading: Icon(Icons.accessibility),
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text(
+                  'Nilai dari perhitungan BMI diklasifikasikan ke dalam 6 kelompok kategori'),
+              leading: Icon(Icons.accessibility),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Text(
+              'Klasifikasi nilai BMI',
+              textAlign: TextAlign.center,
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          Card(
+            color: Colors.yellow,
+            child: ListTile(
+              title: Text('UNDERWEIGHT\nNilai BMI <18.5'),
+              leading: Icon(Icons.adb),
+            ),
+          ),
+          Card(
+            color: Colors.greenAccent,
+            child: ListTile(
+              title: Text('NORMAL\n18.5 ≤ Nilai BMI < 25.0'),
+              leading: Icon(Icons.adb),
+            ),
+          ),
+          Card(
+            color: Colors.yellow,
+            child: ListTile(
+              title: Text('OVERWEIGHT\n25 ≤ Nilai BMI < 30.0'),
+              leading: Icon(Icons.adb),
+            ),
+          ),
+          Card(
+            color: Colors.amber,
+            child: ListTile(
+              title: Text('CLASS I OBESITY\n30 ≤ Nilai BMI < 35.0'),
+              leading: Icon(Icons.adb),
+            ),
+          ),
+          Card(
+            color: Colors.orange,
+            child: ListTile(
+              title: Text('CLASS II OBESITY\n35 ≤ Nilai BMI < 40.0'),
+              leading: Icon(Icons.adb),
+            ),
+          ),
+          Card(
+            color: Colors.redAccent,
+            child: ListTile(
+              title: Text('CLASS III OBESITY\nNilai BMI ≥ 40.0'),
+              leading: Icon(Icons.adb),
+            ),
+          ),
+        ],
+      ),
+    ));
+  }
+}
+
+class GlossaryScreenGrid extends StatelessWidget {
+  final int gridCount;
+
+  const GlossaryScreenGrid({super.key, required this.gridCount});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: GridView.count(
+      crossAxisCount: gridCount,
+      padding: const EdgeInsets.all(40),
+      crossAxisSpacing: 20,
+      mainAxisSpacing: 15,
+      children: <Widget>[
+        Card(
+          child: ListTile(
+            title: Text(
+                'BMI merupakan kependekan dari Body Mass Index atau Index Masa Tubuh'),
+            leading: Icon(Icons.accessibility),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text(
+                'BMI dihitung dari pembagian berat badan (dalam kg) dengan tinggi badan (dalam m)'),
+            leading: Icon(Icons.accessibility),
+          ),
+        ),
+        Card(
+          child: ListTile(
+            title: Text(
+                'Nilai dari perhitungan BMI diklasifikasikan ke dalam 6 kelompok kategori'),
+            leading: Icon(Icons.accessibility),
+          ),
+        ),
+        Card(
+          color: Colors.yellow,
+          child: ListTile(
+            title: Text(
+                'BMI dikategorikan UNDERWEIGHT jika nilainya di bawah 18.5'),
+            leading: Icon(Icons.adb),
+          ),
+        ),
+        Card(
+          color: Colors.greenAccent,
+          child: ListTile(
+            title: Text(
+                'BMI dikategorikan NORMAL jika nilainya di antara 18.6 sampai 25.0'),
+            leading: Icon(Icons.adb),
+          ),
+        ),
+        Card(
+          color: Colors.yellow,
+          child: ListTile(
+            title: Text(
+                'BMI dikategorikan OVERWEIGHT jika nilainya di antara 25.1 sampai 30.0'),
+            leading: Icon(Icons.adb),
+          ),
+        ),
+        Card(
+          color: Colors.amber,
+          child: ListTile(
+            title: Text(
+                'BMI dikategorikan CLASS I OBESITY jika nilainya di antara 30.1 sampai 35.0'),
+            leading: Icon(Icons.adb),
+          ),
+        ),
+        Card(
+          color: Colors.orange,
+          child: ListTile(
+            title: Text(
+                'BMI dikategorikan CLASS II OBESITY jika nilainya di antara 35.1 sampai 40.0'),
+            leading: Icon(Icons.adb),
+          ),
+        ),
+        Card(
+          color: Colors.redAccent,
+          child: ListTile(
+            title: Text(
+                'BMI dikategorikan CLASS II OBESITY CLASS III OBESITY jika nilainya di atas 40.0'),
+            leading: Icon(Icons.adb),
+          ),
+        ),
+      ],
+    ));
   }
 }
--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -9,16 +9,115 @@ class MainScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Kalkulator BMI', style: TextStyle(color: Colors.white),),
+        title: Text(
+          'Kalkulator BMI',
+          style: TextStyle(color: Colors.white),
+        ),
         backgroundColor: Colors.deepPurpleAccent,
       ),
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              ElevatedButton(
+      body: LayoutBuilder(
+          builder: (BuildContext context, BoxConstraints constraints) {
+        if (constraints.maxWidth <= 600)
+          return MainScreenVertical();
+        else
+          return MainScreenHorizontal();
+      }),
+    );
+  }
+}
+
+class MainScreenVertical extends StatelessWidget {
+  const MainScreenVertical({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => CalculateScreen()),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Icon(
+                      Icons.add_chart_sharp,
+                      size: 150,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 32.0),
+                      child: Text(
+                        'Hitung BMI',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                )),
+            SizedBox(
+              height: 30, // <-- SEE HERE
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20))),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => GlossaryScreen()),
+                  );
+                },
+                child: Column(
+                  children: [
+                    Icon(
+                      Icons.abc_sharp,
+                      size: 150,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 32.0),
+                      child: Text(
+                        'Glosarium',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ],
+                ))
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class MainScreenHorizontal extends StatelessWidget {
+  const MainScreenHorizontal({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              width: 300,
+              height: 300,
+              child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
+                      minimumSize: Size(200, 500),
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(20))),
                   onPressed: () {
@@ -28,6 +127,8 @@ class MainScreen extends StatelessWidget {
                     );
                   },
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       Icon(
                         Icons.add_chart_sharp,
@@ -38,16 +139,23 @@ class MainScreen extends StatelessWidget {
                         child: Text(
                           'Hitung BMI',
                           textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 20,),
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
                         ),
                       ),
                     ],
                   )),
-              SizedBox(
-                height: 30, // <-- SEE HERE
-              ),
-              ElevatedButton(
+            ),
+            SizedBox(
+              height: 30, // <-- SEE HERE
+            ),
+            Container(
+              width: 300,
+              height: 300,
+              child: ElevatedButton(
                   style: ElevatedButton.styleFrom(
+                    minimumSize: Size(200, 500),
                       shape: RoundedRectangleBorder(
                           borderRadius: BorderRadius.circular(20))),
                   onPressed: () {
@@ -57,6 +165,8 @@ class MainScreen extends StatelessWidget {
                     );
                   },
                   child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
                     children: [
                       Icon(
                         Icons.abc_sharp,
@@ -67,13 +177,15 @@ class MainScreen extends StatelessWidget {
                         child: Text(
                           'Glosarium',
                           textAlign: TextAlign.center,
-                          style: TextStyle(fontSize: 20,),
+                          style: TextStyle(
+                            fontSize: 20,
+                          ),
                         ),
                       ),
                     ],
-                  ))
-            ],
-          ),
+                  )),
+            )
+          ],
         ),
       ),
     );
