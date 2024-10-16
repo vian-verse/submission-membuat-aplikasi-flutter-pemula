2cd6d01f6aa882978a216ee484d5b73361d8d07c
vian <vianv31253@gmail.com>
2024-10-16 13:43:58 +0700

    menambah filter input, validator, memperbaiki perhitungan bmi, menampilkan bmi 2 angka belakang koma

--- lib/calculate_screen.dart
+++ lib/calculate_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 class CalculateScreen extends StatefulWidget {
   const CalculateScreen({super.key});
@@ -8,8 +11,13 @@ class CalculateScreen extends StatefulWidget {
 }
 
 class _CalculateScreenState extends State<CalculateScreen> {
-  double bmi = 0;
-  String kategori = "-";
+  var bmi = 0.0;
+  var kategori = "-";
+  final formKey = GlobalKey<FormState>();
+
+  TextEditingController cBerat = TextEditingController();
+  TextEditingController cTinggi = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,63 +28,92 @@ class _CalculateScreenState extends State<CalculateScreen> {
         ),
         backgroundColor: Colors.deepPurpleAccent,
       ),
-      body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Text(
-              'Hitung nilai body mass index',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+      body: Form(
+        key: formKey,
+        child: SafeArea(
+            child: Padding(
+          padding: EdgeInsets.all(16.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text(
+                  'Hitung nilai body mass index',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                ),
+                SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  controller: cBerat,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1),
+                          borderRadius: BorderRadius.circular(20)),
+                      labelText: 'Berat badan (kg)',
+                      hintText: 'Berat badan (kg)'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "nilai berat kosong";
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                TextFormField(
+                  controller: cTinggi,
+                  keyboardType: TextInputType.number,
+                  inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                  decoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderSide: BorderSide(width: 1),
+                          borderRadius: BorderRadius.circular(20)),
+                      labelText: 'Tinggi badan (cm)',
+                      hintText: 'Tinggi badan (cm)'),
+                  validator: (value) {
+                    if (value!.isEmpty) {
+                      return "nilai tinggi kosong";
+                    }
+                  },
+                ),
+                SizedBox(
+                  height: 30,
+                ),
+                ElevatedButton(
+                    onPressed: () {
+                      if (formKey.currentState!.validate()) {
+                        bmi = int.parse(cBerat.text) /
+                            pow((int.parse(cTinggi.text) / 100),2);
+                        setState(() {
+                          bmi;
+                        });
+                      }
+                    },
+                    style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.deepPurpleAccent,
+                        minimumSize: const Size(140, 50),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(5)),
+                        shadowColor: Colors.blue,
+                        elevation: 10),
+                    child: Text(
+                      'Hitung',
+                      style: TextStyle(color: Colors.white),
+                    )),
+                SizedBox(
+                  height: 40,
+                ),
+                Text(
+                  'BMI: ${bmi.toStringAsFixed(2)} \n\nKategori: $kategori',
+                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
+                )
+              ],
             ),
-            SizedBox(
-              height: 20,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(width: 1),
-                      borderRadius: BorderRadius.circular(20)),
-                  labelText: 'Berat badan (kg)',
-                  hintText: 'Berat badan (kg)'),
-            ),
-            SizedBox(
-              height: 15,
-            ),
-            TextFormField(
-              decoration: InputDecoration(
-                  border: OutlineInputBorder(
-                      borderSide: BorderSide(width: 1),
-                      borderRadius: BorderRadius.circular(20)),
-                  labelText: 'Tinggi badan (cm)',
-                  hintText: 'Tinggi badan (cm)'),
-            ),
-            SizedBox(
-              height: 30,
-            ),
-            ElevatedButton(
-                onPressed: () {},
-                style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.deepPurpleAccent,
-                    minimumSize: const Size(140, 50),
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(5)),
-                    shadowColor: Colors.blue,
-                    elevation: 10),
-                child: Text(
-                  'Hitung',
-                  style: TextStyle(color: Colors.white),
-                )),
-            SizedBox(
-              height: 40,
-            ),
-            Text(
-              'BMI: $bmi \n\nKategori: $kategori',
-              style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20),
-            )
-          ],
-        ),
-      )),
+          ),
+        )),
+      ),
     );
   }
 }
